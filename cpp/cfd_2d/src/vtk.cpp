53d1c97a51e343e557f76785b165aaeba23196e8
André Glatzl <andre.glatzl@stud.fhgr.ch>
2024-02-18 03:01:04 +0100

    save temp

--- cpp/cfd_2d/src/vtk.cpp
+++ cpp/cfd_2d/src/vtk.cpp
@@ -6,6 +6,7 @@
 #include <vtkDoubleArray.h>
 #include <vtkPointData.h>
 #include <vtkXMLStructuredGridWriter.h>
+#include <vtkXMLUnstructuredGridWriter.h>
 
 void CFD::saveVTK(FluidSimulation* sim) {
     vtkSmartPointer<vtkStructuredGrid> vtk_grid = vtkSmartPointer<vtkStructuredGrid>::New();
@@ -55,5 +56,56 @@ void CFD::saveVTK(FluidSimulation* sim) {
 }
 
 void CFD::saveVTKGeometry(FluidSimulation* sim) {
-    // @todo
+    vtkSmartPointer<vtkUnstructuredGrid> vtk_grid = vtkSmartPointer<vtkUnstructuredGrid>::New();
+
+    int startI = 0x7FFFFFFF;
+    int startJ = 0x7FFFFFFF;
+    int endI = 0;
+    int endJ = 0;
+
+    // Create vtk points
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    for (int j = 0; j < sim->jmax; ++j) {
+        for (int i = 0; i < sim->imax; ++i) {
+            points->InsertNextPoint(i * sim->grid.dx, j * sim->grid.dy, 0.0);  // Assuming 2D, setting z-coordinate to 0.0
+        }
+    }
+
+    for (int j = 1; j < sim->jmax; ++j) {
+        for (int i = 1; i < sim->imax; ++i) {
+            if (sim->isObstacleCell(i, j)) {
+                startI = std::min(startI, i);
+                startJ = std::min(startJ, j);
+                endI = std::max(endI, i);
+                endJ = std::max(endJ, j);
+            }
+        }
+    }
+
+    std::cout << "startI: " << startI << " startJ: " << startJ << std::endl;
+
+    // Create vtk cells for the geometry
+    vtkSmartPointer<vtkCellArray> cells = vtkSmartPointer<vtkCellArray>::New();
+    vtkIdType vertexIds[4];  // Assuming 2D, use 4 vertices for each cell
+    
+    for (int j = startJ; j < endJ; ++j) {
+        for (int i = startI; i < endI; ++i) {
+            vertexIds[0] = i + sim->imax * j;
+            vertexIds[1] = (i + 1) + sim->imax * j;
+            vertexIds[2] = (i + 1) + sim->imax * (j + 1);
+            vertexIds[3] = i + sim->imax * (j + 1);
+            
+            cells->InsertNextCell(4, vertexIds);  // Use 4 for quadrilateral cells in 2D
+        }
+    }
+
+    vtk_grid->SetPoints(points);
+    vtk_grid->SetCells(VTK_QUAD, cells);  // Use VTK_QUAD for 2D quadrilateral cells
+
+    vtkSmartPointer<vtkXMLUnstructuredGridWriter> writer = vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
+    char filename[100];
+    sprintf(filename, "geometry.vtu", sim->it);
+    writer->SetFileName(filename);
+    writer->SetInputData(vtk_grid);
+    writer->Write();
 }
